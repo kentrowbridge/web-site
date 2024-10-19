cbec2ed9e973abb112993604b91488681f3bbea5
Kenny Trowbridge <kenny.trowbridge@gmail.com>
2024-10-19 13:48:46 -0600

    Update page styling and add resume file

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const app = express();
 
 app.use('/', express.static('public'));
 
-app.get('/api', (req, res, next) => {
-    res.json({test: 2});
-});
+// app.get('/api', (req, res, next) => {
+//     res.json({test: 2});
+// });
 
 var port = process.env.PORT || 3000;
 app.listen(port, () =>
-    console.log(`Example app listening on port ${port}!`),
+    console.log(`Listening on port ${port}!`),
 );
